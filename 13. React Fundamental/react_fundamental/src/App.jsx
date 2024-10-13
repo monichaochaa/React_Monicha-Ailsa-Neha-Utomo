c139d343db4db6ac1ebf58e9e629bfe44910387d
monichaochaa <monicaailsa373@gmail.com>
2024-10-13 23:17:59 +0700

    tugas

--- 13. React Fundamental/react_fundamental/src/App.jsx
+++ 13. React Fundamental/react_fundamental/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import CreateProduct from './components/CreateProduct.jsx'; 
 import CreateProductPage from './components/CreateProductPage';
 
 const App = () => {
-    return (
-      <div className="bg-gray-100 min-h-screen flex justify-center items-center">
-        <CreateProductPage />
-        </div>
+  return (
+      <Router>
+          <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+              <Routes>
+                  <Route path="/" element={<CreateProduct />} /> {/* Halaman Utama */}
+                  <Route path="/create-product" element={<CreateProductPage />} /> {/* Halaman Create Product */}
+              </Routes>
+          </div>
+      </Router>
+  );
+};
 
-    );
-  };
-  
-  export default App;
+export default App;
--- 13. React Fundamental/react_fundamental/src/components/CreateProduct.jsx
+++ 13. React Fundamental/react_fundamental/src/components/CreateProduct.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const CreateProduct = () => {
   return (
@@ -75,6 +76,7 @@ const CreateProduct = () => {
           </form>
         </div>
       </section>
+      <Link to="/create-product">Buat Produk Baru</Link>
     </div>
   );
 };
